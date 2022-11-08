486c33878f71fa31841b812a7d295438119ef3b9
vsamaru <vsamaru@gmail.com>
2022-11-08 14:51:20 +0200

    Add files via upload

---
+++ src/paint-by-numbers/random.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+exports.Random = void 0;
+var Random = /** @class */ (function () {
+    function Random(seed) {
+        if (typeof seed === 'undefined') {
+            this.seed = new Date().getTime();
+        }
+        else {
+            this.seed = seed;
+        }
+    }
+    Random.prototype.next = function () {
+        var x = Math.sin(this.seed++) * 10000;
+        return x - Math.floor(x);
+    };
+    return Random;
+}());
+exports.Random = Random;
